22b162592f5b59f4fb32f5dd83255deda2c8bc8d
sheerazspb <sheerazspb@gmail.com>
2019-02-22 20:30:00 +0300

    updated main.js file

---
+++ variables/main.js
@@ -0,0 +1,18 @@
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+let algebra = 5;
+let geography = 4;
+let physics = 3;
+let avgScore = (algebra + geography + physics) / 3;
+
+console.log(avgScore);
+
+let name = 'Sheeraz';
+let message = (`Привет, мир! Меня зовут ${name}`);
+
+console.log(message);
