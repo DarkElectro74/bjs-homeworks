262373a92657e5692eb979ffa4352e320d741747
sheerazspb <sheerazspb@gmail.com>
2019-02-27 15:40:09 +0300

    updated Extended-Syntax/task.js file

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -11,8 +11,21 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+    let discriminant = (b * b) - (4 * a * c);
+    
+      if(discriminant > 0) {
+        let x1 = (-b + discriminant) / (2 * a);
+        let x2 = (-b - discriminant) / (2 * a);
+        let x = [x1,x2];
+        console.log(2 + ' roots ' + x);  
+        return x;    
+    }else if(discriminant === 0) {
+        let x = -b / (2 * a);
+        return x; 
+        console.log(1 + ' root ' + x);
+    }else {
+        console.log('no roots');
+    }
 }
 
 function calculateDrinkTask(){
@@ -23,9 +36,15 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+    let presentYear = new Date().getFullYear();
+    let age = presentYear - dateOfBirthday.getFullYear();
+    console.log(`you are ${age} years old.`);
+    if (age < 18) {
+    console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+  } else {
+    console.log(`Не желаете ли олд-фэшн, ${name}?`);
+  }
+    return result;
 }
 
 function calculateAverageRating(){
@@ -35,6 +54,18 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №3 писать здесь
-    //return averageMark;
+    let avgMarks = 0;
+    let totalMarks = 0;
+    
+    if(marks.length > 5) {
+        console.log(`you have put ${marks.length} subject's scores`);
+        marks = marks.slice(0,5);
+    }
+
+    for(let i=0;i<marks.length;i++) {
+        totalMarks += marks[i];
+    }
+
+    avgMarks = totalMarks / marks.length;
+    return avgMarks;
 }
